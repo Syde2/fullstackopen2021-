92ea41f8519618b450b255449e0b7694b6036a7b
Sylvain Del <s.delmote@protonmail.com>
2021-03-22 21:35:35 +0100

    Starter pattern

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,11 +1,16 @@
+import React, { useState } from 'react'
 
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-function App() {
   return (
-    <div className="App">
-  
+    <div>
+      code here
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
