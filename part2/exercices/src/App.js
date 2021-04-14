40b1d9a0297c5c138f534dc396bf07f34af036fb
Sylvain Del <s.delmote@protonmail.com>
2021-04-14 14:37:31 +0200

    added useEffect and axios request

--- part2/exercices/src/App.js
+++ part2/exercices/src/App.js
@@ -1,17 +1,13 @@
-const App = (props) => {
-  const { notes } = props
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+
+const App = ({notes}) => {
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-       {notes.map(note =>
-       <li key={note.id}>{note.content}</li>
-       
-       )}
-      </ul>
+      
     </div>
   )
 }
-
 export default App
